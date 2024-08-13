6a1343aac29a10d54e4f98c0e7509c1343dbe14b
dinelkathilina <dinelkathilina3@gmail.com>
2024-08-13 20:12:03 +0530

    Latest Model

---
+++ SAMS/Models/AuthModels.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SAMS.Models
+{
+    public class AuthModels
+    {
+        public class RegisterModel
+        {
+            [Required(ErrorMessage = "Name is required")]
+            [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "Email is required")]
+            [EmailAddress(ErrorMessage = "Invalid email address")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Password is required")]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        // LoginModel remains unchanged
+        public class LoginModel
+        {
+            [Required(ErrorMessage = "Email is required")]
+            [EmailAddress(ErrorMessage = "Invalid email address")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Password is required")]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+    }
+}
