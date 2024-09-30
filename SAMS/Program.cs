49dfed19d9081a708a00585c2d173ef4ce37c456
dinelkathilina <dinelkathilina3@gmail.com>
2024-10-01 03:33:13 +0530

    removed comment

--- SAMS/Program.cs
+++ SAMS/Program.cs
@@ -166,7 +166,7 @@ app.MapSessionEndpoints();
 //app.Run($"http://0.0.0.0:{port}");
 
 app.MapHub<AttendanceHub>("/attendanceHub");
-=======
+
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
 app.Run($"http://0.0.0.0:{port}");
 
