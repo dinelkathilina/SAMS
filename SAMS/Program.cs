b1df731a56274299fa27907ca0e9de141346602b
dinelkathilina <dinelkathilina3@gmail.com>
2024-10-05 22:41:40 +0530

    generaate report works

--- SAMS/Program.cs
+++ SAMS/Program.cs
@@ -163,6 +163,7 @@ app.MapUserProfileEndpoints();
 app.UseDeveloperExceptionPage();
 app.MapLecturerEndpoints();
 app.MapSessionEndpoints();
+app.MapAttendanceReportEndpoints();
 
 //var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
 //app.Run($"http://0.0.0.0:{port}");
